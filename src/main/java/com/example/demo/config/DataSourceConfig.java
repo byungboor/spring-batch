739bd6239c2c1f15e34a2babea9d411874e116db
byungboor <byungboor@naver.com>
2020-02-20 19:46:20 -0800

    add todo

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -4,14 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Slf4j
-@Configuration  //TODO-02
+@Configuration
 public class DataSourceConfig {
 
-    // TODO-03
+    // TODO-04 declare @Primary annotation here!
     @Bean(name = "batchDataSource")
     public DataSource batchDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
@@ -22,4 +23,16 @@ public class DataSourceConfig {
         return dataSource;
     }
 
+
+    @Bean(name = "") // TODO - 01 set spring bean name as backupDataSource
+    public DataSource backupDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        //TODO-02 set url  of ad_batch_backup database
+        dataSource.setUrl("");
+        dataSource.setUsername("dev_user_backup");
+        // TODO-03 set password
+        dataSource.setPassword("1q2w3e4r");
+        return dataSource;
+    }
 }
